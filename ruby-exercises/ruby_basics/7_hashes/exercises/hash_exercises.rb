911296d5803eb714b23c5c9b6fe4c5dded0db4aa
Michael Brockman <mbrockmandev@gmail.com>
2023-09-03 14:19:23 -0400

    add first rails app

--- ruby-exercises/ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby-exercises/ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -2,6 +2,7 @@ def create_favorite_hash(color, number)
   # return a hash with the following key/value pairs:
   # key of color (as a symbol) with value of the color argument
   # key of number (as a symbol) with the value of the number argument
+  
 end
 
 def favorite_color(favorite_list)
@@ -12,7 +13,7 @@ def favorite_number(favorite_list)
   # use #fetch to return the value of the number key or 42 if the key is not found
 end
 
-def update_favorite_movie(favorite_list, movie)
+def update_favorite_movie(favorite_list, _movie)
   # Step 1: add/update the key of movie (as a symbol)
 
   # Step 2: return the hash (because Step 1 returns the value of the movie key)
